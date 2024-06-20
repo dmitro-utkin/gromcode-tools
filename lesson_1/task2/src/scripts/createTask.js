1c6fc9da11ce114aa85aa000f97385250395ad25
dmitro-utkin <sssddd1366628@gmail.com>
2024-06-20 12:33:31 +0300

    try to do les1/task 2

--- lesson_1/task2/src/scripts/createTask.js
+++ lesson_1/task2/src/scripts/createTask.js
@@ -26,8 +26,3 @@ export const onCreateTask = () => {
     });
 };
 
-// 1. Prepare data
-// 2. Write data to db
-// 3. Read new data from server
-// 4. Save new data to front-end storage
-// 5. Update UI based on new data
