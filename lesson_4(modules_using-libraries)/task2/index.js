32987e96029a02768b747378f1faca752d0000ab
dmitro-utkin <sssddd1366628@gmail.com>
2024-06-23 11:10:50 +0300

    try to do task2 from les4

--- lesson_4(modules_using-libraries)/task2/index.js
+++ lesson_4(modules_using-libraries)/task2/index.js
@@ -1,19 +1,19 @@
 // revealing module pattern
 
-// const store = (function() {
-//   let storage = {};
-//   return {
-//     addItem: function( key, value ) {
-//       Object.assign( storage, { [key]: value } );
-//     }, 
-//     clear: function() {
-//       storage = {};
-//     }
-//   };
-// })();
+const store = (function() {
+  let storage = {};
+  return {
+    addItem: function( key, value ) {
+      Object.assign( storage, { [key]: value } );
+    }, 
+    clear: function() {
+      storage = {};
+    }
+  };
+})();
 
-// console.log(store);
-// console.log(storage);
+console.log(store);
+console.log(storage);
 
 
 //----------------------------
