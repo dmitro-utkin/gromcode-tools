65b2df213983961cd8a1379c76cbf6a656b9ac3c
dmitro-utkin <sssddd1366628@gmail.com>
2024-06-23 10:35:51 +0300

    try to do lesson_4 modules

---
+++ lesson_4(modules_using-libraries)/task2/index.js
@@ -0,0 +1,41 @@
+// revealing module pattern
+
+// const store = (function() {
+//   let storage = {};
+//   return {
+//     addItem: function( key, value ) {
+//       Object.assign( storage, { [key]: value } );
+//     }, 
+//     clear: function() {
+//       storage = {};
+//     }
+//   };
+// })();
+
+// console.log(store);
+// console.log(storage);
+
+
+//----------------------------
+
+// (function func() {
+//   let a = 17;
+
+//   console.log(a);
+// })()
+
+// (function func() {
+  // let a = 17;
+
+  // console.log(a);
+// })()
+
+// Imidiately invoked function expression (IIFE) 
+
+// ----------------------------------------------
+// ----------------------------------------------
+// ----------------------------------------------
+
+// assynchronous modules defenition
+// AMD + require.js
+
