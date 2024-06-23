0a5b4c1ef29506075eaac4758e62b37dc7c768c1
dmitro-utkin <sssddd1366628@gmail.com>
2024-06-23 12:07:19 +0300

    try to fix bugs

--- lesson_4(modules_using-libraries)/task1/callculator/index.js
+++ lesson_4(modules_using-libraries)/task1/callculator/index.js
@@ -3,15 +3,11 @@
 export const sum = (a, b) => {
   console.log('implementation for sum');
   // ... implementation
-
-  return a + b
 };
 
 export const mult = (a, b) => {
   console.log('implementation for mult');
   // ... implementation
-
-  return a * b
 };
 
 // экспортируйте calc как export по умолчанию
@@ -21,11 +17,9 @@ export const mult = (a, b) => {
 //   // ... implementation
 // };
 
-export default expression => {
+export default calcExpression => {
   console.log('implementation for calc');
   // ... implementation
-
-  return eval(expression)
 };
 
 // export default calc; // дефолтний експорт за замовчуванням можна
