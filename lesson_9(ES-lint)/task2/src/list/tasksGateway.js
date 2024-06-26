81e144f1ac95dbf25f18ecc7f48d137b3a71c372
dmitro-utkin <sssddd1366628@gmail.com>
2024-06-26 20:39:14 +0300

    try todo les9/task2

---
+++ lesson_9(ES-lint)/task2/src/list/tasksGateway.js
@@ -0,0 +1,27 @@
+const baseUrl = 'https://662630a1052332d55321f809.mockapi.io/api/v1/tasks';
+
+const mapTasks = (tasks) => tasks.map(({ id, ...task }) => ({ ...task, id }));
+
+export const getTasksList = () => fetch(baseUrl)
+  .then((response) => response.json())
+  .then((tasks) => mapTasks(tasks));
+
+export const createTask = (taskData) => fetch(baseUrl, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json;charset=utf-8',
+  },
+  body: JSON.stringify(taskData),
+});
+
+export const updateTask = (taskId, updatedTaskData) => fetch(`${baseUrl}/${taskId}`, {
+  method: 'PUT',
+  headers: {
+    'Content-Type': 'application/json;charset=utf-8',
+  },
+  body: JSON.stringify(updatedTaskData),
+});
+
+export const deleteTask = (taskId) => fetch(`${baseUrl}/${taskId}`, {
+  method: 'DELETE',
+});
